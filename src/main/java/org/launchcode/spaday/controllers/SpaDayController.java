5e42de3ed292f3fa1a05fca808524367426ad0df
bsavanth <bsavanth@uark.edu>
2021-02-22 20:01:21 -0600

    added logic to response form

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -69,6 +69,11 @@ public class SpaDayController {
     @PostMapping(value="")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
 
+
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+
         ArrayList<String> facials = new ArrayList<String>();
         facials.add("Microdermabrasion");
         facials.add("Hydrofacial");
@@ -81,6 +86,18 @@ public class SpaDayController {
                 appropriateFacials.add(facials.get(i));
             }
         }
+
+        model.addAttribute("appropriateFacials", appropriateFacials);
+        System.out.println("\n"+manipedi+"\n");
+
+
+        //object model
+//        {
+//             "name" : name;
+//             "skintype": skintype;
+//             "manipedi" : manipedi;
+//             "appropriatefacials" : appropriateFacials;
+//        }
 
         return "menu";
     }
